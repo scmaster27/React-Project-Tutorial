4fb343e2a69b5ddb98db0421db7376e9578dcbc6
Github Study <scmaster27@gmail.com>
2019-08-30 15:39:13 +0900

    #18

--- management/client/src/components/CustomerAdd.js
+++ management/client/src/components/CustomerAdd.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+});
 
 class CustomerAdd extends React.Component {
     constructor(props){
@@ -10,7 +23,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         }
     }
 
@@ -27,7 +41,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
     }
 
@@ -60,19 +75,62 @@ class CustomerAdd extends React.Component {
         return post(url, formData, config);
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
+        })
+    }
+
     render(){
+        const { classes } = this.props;
         return (
-            <form onSubmit={this.handleFormSubmit}>
-                <h1>顧客追加</h1>
-                プロフィルイメージ：<input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}  /><br />
-                名前：<input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br />
-                生年月日：<input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}  /><br />
-                性別：<input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}  /><br />
-                職業：<input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}  /><br />
-                <button type="submit">追加</button>
-            </form>
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    顧客追加
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>顧客追加</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}  /><br />
+                        <label htmlFor="raised-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "プロフィール イメージ選択" : this.state.fileName}
+                            </Button>
+                        </label><br />
+                        <TextField label="名前" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br />
+                        <TextField label="生年月日" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}  /><br />
+                        <TextField label="性別" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}  /><br />
+                        <TextField label="職業" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}  /><br />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>追加</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>閉じる</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+            // <form onSubmit={this.handleFormSubmit}>
+            //     <h1>顧客追加</h1>
+            //     プロフィルイメージ：<input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}  /><br />
+            //     名前：<input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br />
+            //     生年月日：<input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}  /><br />
+            //     性別：<input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}  /><br />
+            //     職業：<input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}  /><br />
+            //     <button type="submit">追加</button>
+            // </form>
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- management/client/src/components/CustomerDelete.js
+++ management/client/src/components/CustomerDelete.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-class CustomerDelete extends React.Component{
+class CustomerDelete extends React.Component {
 
-    deleteCustomer(id){
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
+
+    deleteCustomer(id) {
         const url = '/api/customers/' + id;
         fetch(url, {
             method: 'DELETE'
@@ -12,7 +37,23 @@ class CustomerDelete extends React.Component{
 
     render(){
         return (
-            <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>削除</button>
+            <div>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>削除</Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle onClose={this.handleClose}>
+                        削除 警告
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBotton>
+                            選択した顧客情報が削除されます。
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={(e) => { this.deleteCustomer(this.props.id) }}>削除</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>閉じる</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         )
     }
 }
